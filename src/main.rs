55733b39c08a9fa4066810447fdb9bf244f43e87
Justin K <justin.kolaras0@gmail.com>
2023-03-19 15:37:54 -0700

    Use template rendering for results page

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,62 @@ extern crate rocket;
 use rocket::form::Form;
 use rocket::fs::NamedFile;
 use rocket::request::Request;
+use rocket_dyn_templates::{context, Template};
 
-use percentages::{run, CalcResult};
+use percentages::run;
 
 #[derive(FromForm)]
 struct Equation {
     equation: String,
 }
 
+/// GET.
+
 #[get("/")]
-async fn index() -> Option<NamedFile> {
+async fn home() -> Option<NamedFile> {
     NamedFile::open("static/index.html").await.ok()
 }
 
-#[post("/submit", data = "<eq>")]
-async fn submit(eq: Form<Equation>) -> String {
+// Manually add a route to return the CSS file.
+#[get("/style.css")]
+async fn style() -> Option<NamedFile> {
+    NamedFile::open("static/style.css").await.ok()
+}
+
+/// POST.
+
+#[post("/results", data = "<eq>")]
+async fn results(eq: Form<Equation>) -> Template {
     let eq: Equation = eq.into_inner();
     let eq: String = eq.equation;
     let eq: &str = eq.trim();
 
-    let result: Result<CalcResult, String> = run(eq.to_string());
+    let results: String = match run(eq.to_string()) {
+        Ok(result) => format!("Elements: {}\nResult: {}%", result.elements, result.result),
+        Err(err) => format!("Computation error.\n\n{}", err),
+    };
 
-    match result {
-        Ok(result) => format!("Elements: {}\nResult: {}", result.elements, result.result),
-        Err(err) => err,
-    }
+    Template::render(
+        "results",
+        context! {
+            results,
+        },
+    )
 }
+
+/// Catchers.
 
 #[catch(404)]
 fn not_found(req: &Request<'_>) -> String {
     format!("Sorry, `{}` is not a valid path.", req.uri())
 }
 
+/// Launch.
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, submit])
         .register("/", catchers![not_found])
+        .mount("/", routes![home, results, style])
+        .attach(Template::fairing())
 }
